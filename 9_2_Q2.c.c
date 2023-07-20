8842e5644de793bde069286236578945ea7b4d3b
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-07-20 12:05:07 +0530

    Add files via upload

---
+++ 9_2_Q2.c.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void main(){
+
+char oldemail[50];
+char oldpassword[20];
+
+int i,l;
+
+printf("enetr the email");
+gets("oldmail");
+
+printf("\n enter your email");
+gets("oldmail");
+
+printf("write your passwprd :-");
+gets("oldpassword");
+
+if(strcmp(oldmail,"jenish@gamil.com")==0)
+
+{
+
+	if(strcmp(oldpassword,jenish#123)==0)
+	
+{
+	printf("\n you are loged in ");	
+}
+	
+
+}else{
+	
+	printf("\n login failed please try again");
+
+}
+		
+
